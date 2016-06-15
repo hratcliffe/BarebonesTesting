649277ac1a8a7f9b67d3da5d5e75d1207555b74b
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2016-06-15 12:28:53 +0100

    Rearranged code

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 
+
 #include <stdio.h>
 #include "tests.h"
+//#include "test_min.h"
+
 int main(int argc, char ** argv){
 
 
-
-mpi_info_struc aa={10, 0}, bb={0,0};
+mpi_info_struc aa={10, 0};
+mpi_info_struc bb={0,0};
 
 std::cout<<(aa==mpi_info_null)<<'\n';
 std::cout<<(bb==mpi_info_null)<<'\n';
 
-tests * test_bed=new tests();
+//tests * test_bed=new tests();
 
+int i=100;
+double d=123e3;
+std::cout<<testbed::mk_str(i)<<" "<<testbed::mk_str(d)<<" "<<testbed::mk_str(d, true)<<'\n';
 
-
-
+//" "<<mk_str(d, true)
 
 
 
--- tests.h
+++ tests.h
@@ -3,6 +3,7 @@
 //  
 //
 //  Created by Heather Ratcliffe on 10/11/2015.
+// Modified 15/6/2016
 //
 //
 #ifdef RUN_TESTS_AND_EXIT
@@ -14,6 +15,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <string>
 
 #define calc_type double
 
@@ -25,8 +27,16 @@ struct mpi_info_struc{
 
 const struct mpi_info_struc mpi_info_null = {0, 0};
 
-const int TEST_PASSED = 0;
-const int TEST_WRONG_RESULT = 1;
+namespace testbed{
+
+#ifdef USEMPI
+  const mpi_info_struc mpi_info={1, 2};
+#else
+  const mpi_info_struc mpi_info = mpi_info_null;
+#endif
+
+  const int TEST_PASSED = 0;
+  const int TEST_WRONG_RESULT = 1;
 const int TEST_NULL_RESULT = 2;
 const int TEST_ASSERT_FAIL = 4;
 const int TEST_USERDEF_ERR1 = 8;
@@ -40,23 +50,88 @@ const calc_type LOW_PRECISION = 5e-3;/**< Constant for equality at low precision
 const int max_verbos = 4;
 const std::string filename = "tests.log";/**<Test log file*/
 
-namespace testbed{
 
-  void my_print(std::string text, int rank, int rank_to_write=0);
-  void my_print(std::fstream * handle, std::string text, int rank, int rank_to_write=0);
-  void my_print(std::string text);
-  void my_print(std::fstream * handle, std::string text);
-  //Wrapper ensuring only one MPI rank writes
+const int err_codes[err_tot] ={TEST_PASSED, TEST_WRONG_RESULT, TEST_NULL_RESULT, TEST_ASSERT_FAIL, TEST_USERDEF_ERR1, TEST_USERDEF_ERR2, TEST_USERDEF_ERR3, TEST_USERDEF_ERR4};/**< List of error codes available*/
 
-  std::string mk_str(int i);/**<Converts int to string*/
-  std::string mk_str(bool b);/**<Converts bool to string*/
-  //std::string mk_str(size_t i){ return mk_str((int) i);} /**<Converts size_t to string*/
-  std::string mk_str(double i, bool noexp=0);/**<Converts double to string*/
-  std::string mk_str(float i, bool noexp=0);/**<Converts float to string*/
-  std::string mk_str(long double i, bool noexp=0);/**<Converts long double to string*/
-  void trim_string(std::string &str, char ch=' '); /**< Trim all leading/trailing ch's from str*/
-};
+std::string err_names[err_tot]={"None", "Wrong result", "Invalid Null result", "Assignment or assertion failed", "", "", "", ""};/**< Names corresponding to error codes, which are reported in log files*/
 
+  //Wrapper functions ensuring only one MPI rank writes
+inline void my_print(std::string text, int rank, int rank_to_write=0){
+/** \brief Write output
+*
+* Currently dump to term. Perhaps also to log file. Accomodates MPI also. Set rank_to_write to -1 to dump from all. Default value is 0
+*/
+  if(rank == rank_to_write || rank_to_write == -1){
+    std::cout<< text<<std::endl;
+  }
+
+}
+inline void my_print(std::fstream * handle, std::string text, int rank, int rank_to_write=0){
+/** \brief Write output
+*
+* Currently dump to term. Perhaps also to log file. Accomodates MPI also. Set rank_to_write to -1 to dump from all. Default value is 0
+*/
+  if((rank == rank_to_write || rank_to_write == -1) && handle!=nullptr){
+    *handle<<text<<std::endl;
+  }else if(rank == rank_to_write || rank_to_write == -1){
+    std::cout<<text<<std::endl;
+
+  }
+
+}
+inline void my_print(std::string text){
+    std::cout<< text<<std::endl;
+}
+inline void my_print(std::fstream * handle, std::string text){
+  if(handle!=nullptr){
+    *handle<<text<<std::endl;
+  }else{
+    std::cout<<text<<std::endl;
+  }
+
+}
+
+ void trim_string(std::string &str, char ch=' '); /**< Trim all leading/trailing ch's from str*/
+
+#ifdef USECPP11
+//We have to_string, just wrap that for numeric types
+
+template <typename T> std::string mk_str(T input){
+  return std::to_string(input);
+}
+
+template <typename T> std::string mk_str(T input, bool noexp){
+  return std::to_string(input);
+}
+
+#else
+inline std::string mk_str(int i){
+
+  char buffer[25];
+  std::sprintf(buffer, "%i", i);
+  std::string ret = buffer;
+  return ret;
+  
+}
+inline std::string mk_str(double i, bool noexp=0){
+
+  char buffer[25];
+  if(noexp) std::snprintf(buffer, 25, "%f", i);
+  else std::snprintf(buffer, 25, "%e", i);
+  std::string ret = buffer;
+  return ret;
+  
+}
+inline std::string mk_str(bool b){
+
+  if(b) return "1";
+  else return "0";
+
+}
+inline std::string mk_str(long double i, bool noexp){return mk_str((double) i, noexp);};
+inline std::string mk_str(float i, bool noexp){return mk_str((double) i, noexp);};
+
+#endif
 
 /**\brief Testing instance
 *
@@ -79,30 +154,170 @@ public:
 *Descend an object from test_entity which has at least a constructor doing any setup required, a name string for output id, a function run taking no parameters which performs the necessary test and a destructor doing cleanup. Add any other member variables or functions required, including their headers also. In tests::setup_tests create an instance of your class as test_obj = new your_class() and then add your test to the remit using add_test(test_obj); Alternately make the instance and use the global test_bed using test_bed->add(your pntr) from anywhere.
 *To add errors, add the message into the blank spaces in the list below, err_names, and declare a const int of desired name aliased to TEST_USERDEF_ERR* where * = 1-4
 *To report the errors by code, call test_bed->report_err(err); To report other salient information use test_bed->report_info(info, verbosity) where the second parameter is an integer describing the verbosity setting at which to print this info (0=always, the larger int means more and more detail).
-
-
 */
 
 class tests{
 private:
 
-  std::string get_printable_error(int err, int test_id);
+/** \brief Make an error message
+*
+* Converts error code to printable string, adds code for reference and adds test name. Note code is bitmask and additional errors are appended together
+*/
+  std::string get_printable_error(int err, int test_id){
+  std::string err_string="";
+  if(err!=TEST_PASSED){
+    for(int i=err_tot-1; i>0; --i){
+      //Run most to least significant
+      if((err & err_codes[i]) == err_codes[i]){
+        err_string +=err_names[i] + ", ";
+      }
+    }
+  
+    err_string = "Error "+err_string+"(code "+mk_str(err)+") on";
+  }
+  else err_string = "Passed";
+  return err_string+" test "+test_list[test_id]->name;
+
+}
+
   std::fstream * outfile; /**< Output file handle*/
   int current_test_id;/**< Number in list of test being run*/
   std::vector<test_entity*> test_list;/**< List of tests to run*/
   int verbosity;/**< Verbosity level of output*/
 public:
-  void report_err(int err, int test_id=-1);
-  void report_info(std::string info, int verb_to_print = 1, int test_id=-1);
-  tests();
-  ~tests();
-  void setup_tests();
-  void add_test(test_entity* test);
-  void cleanup_tests();
-  void run_tests();
-  void set_verbosity(int verb);
-  void set_colour(char col=0);
+
+/** \brief Log error
+*
+* Logs error text corresponding to code err for test defined by test_id. Errors are always recorded.*/
+
+  void report_err(int err, int test_id=-1){
+  if(test_id == -1) test_id = current_test_id;
+  if(err ==TEST_PASSED) set_colour('b');
+  else set_colour('r');
+  my_print(outfile, get_printable_error(err, test_id), mpi_info.rank);
+  my_print(nullptr, get_printable_error(err, test_id), mpi_info.rank);
+  set_colour();
+
+}
+
+  /** \brief Log other test info
+  *
+  *Records string info to the tests.log file and to screen, according to requested verbosity.
+  */
+  void report_info(std::string info, int verb_to_print = 1, int test_id=-1){
+  if(test_id == -1) test_id = current_test_id;
+  if(verb_to_print <= this->verbosity){
+    my_print(outfile, info, mpi_info.rank);
+    my_print(nullptr, info, mpi_info.rank);
+  }
+};
+
+  tests(){setup_tests();};
+  ~tests(){cleanup_tests();};
+  void setup_tests(){
+/** \brief Setup test bed
+*
+*Opens reporting file. Then instantiates all the test objects and adds them into the test_list
+*/
+  outfile = new std::fstream();
+  outfile->open(filename.c_str(), std::ios::out);
+  if(!outfile->is_open()){
+    my_print("Error opening "+filename, mpi_info.rank);
+    //can't log so return with empty test list
+    return;
+  }
+}
+  /** Adds a test to the list to be performed*/
+  void add_test(test_entity* test){test_list.push_back(test);};
+  /** \brief Delete test objects
+  *
+  *
+  */
+  void cleanup_tests(){
+  if(outfile->is_open()){
+    this->report_info("Testing complete and logged in " +filename, 0);
+    outfile->close();
+  }else{
+    this->report_info("No logfile generated", 0);
+  }
+  delete outfile;
+  for(current_test_id=0; current_test_id< (int)test_list.size(); current_test_id++){
+    delete test_list[current_test_id];
   
+  }
+  
+};
+  /** \brief Run scheduled tests
+  *
+  *Runs each test in list and reports total errors found
+  */
+  void run_tests(){
+  int total_errs = 0;
+  for(current_test_id=0; current_test_id< (int)test_list.size(); current_test_id++){
+    total_errs += (bool) test_list[current_test_id]->run();
+    //Add one if is any error returned
+  }
+  this->set_colour('r');
+  this->report_info(mk_str(total_errs)+" failed tests", mpi_info.rank);
+  this->set_colour();
+
+};
+  /** Set the verbosity of testing output, from 0 (minimal) to max_verbos.*/
+  void set_verbosity(int verb){if((verb > 0)) this->verbosity = std::max(verb, max_verbos);};
+
+  /** \brief Set output text colour*/
+  void set_colour(char col=0){my_print(this->get_color_escape(col));};
+
+  std::string get_color_escape(char col=0);
+  
+};
+
+//Break this out because it's giant case
+inline std::string tests::get_color_escape(char col){
+/** \brief Get color string
+*
+* Returns the terminal escape string to set given colour
+*Set terminal output colour using std escape sequences. Accepts no argument to return to default, or rgb, cmyk and white to test text colour. NB technically not MPI safe. Use sparingly to highlight important information.
+
+*/
+  switch (col) {
+    case 0:
+      return "\033[0m";
+      break;//Redundant but clearer
+    case 'r':
+      return "\033[31m";
+      break;
+    case 'g':
+      return "\033[32m";
+      break;
+    case 'b':
+      return "\033[34m";
+      break;
+    case 'c':
+      return "\033[36m";
+      break;
+    case 'm':
+      return "\033[35m";
+      break;
+    case 'y':
+      return "\033[33m";
+      break;
+    case 'w':
+      return "\033[37m";
+      break;
+    case 'k':
+      return "\033[30m";
+      break;
+
+    default:
+      return "";
+  }
+
+
+
+}
+
+
 };
 
 #endif
